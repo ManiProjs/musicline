0ab9d77dea9a4c5401661278f0dfddecd4e93abe
Mani Arasteh <mani.arasteh92@gmail.com>
2025-04-13 17:36:18 +0330

    Add GitHub Actions workflow for deploying to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/musicline",
+  output: "export",
+  reactStrictMode: false,
 };
 
 export default nextConfig;
