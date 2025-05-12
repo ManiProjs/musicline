7cb14095f68d66fc0b3f4376f30a81b8875ae22b
Mani Arasteh <mani.arasteh92@gmail.com>
2025-05-12 19:12:39 +0330

    MLSetup, running musicline in web and some more things

---
+++ run_in_web.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start Musicline in a browser!
+# Yes, it's a thing; with Textual.
+
+green() {
+  printf "\x1b[1;32m$1\x1b[0;0m"
+}
+
+blue() {
+  printf "\x1b[1;34m$1\x1b[0;0m"
+}
+
+red() {
+  printf "\x1b[1;31m$1\x1b[0;0m"
+}
